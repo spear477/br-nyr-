18d62b253feef4af93f4192613bea87a8123ec7b
Aung Ko <aungko@Aungs-MacBook-Pro-2.local>
2023-09-25 21:58:14 +0700

    d-5 added

---
+++ file-loop.py
@@ -0,0 +1,12 @@
+def main():
+    fileName = input("What file are the numbers in? ")
+    infile = open(fileName,'r')
+    sum = 0.0
+    count = 0
+    for line in infile.readlines():
+        print(line)
+        sum = sum + eval(line)
+        count = count + 1
+    print("\nThe average of the numbers is", sum / count)
+    
+main()    
---
+++ nested-loop.py
@@ -0,0 +1,24 @@
+def nested_loop():
+    
+    no_stu = eval(input("Type no of students -"))
+    no_sub = eval(input("Type no of subjects -"))
+    
+    for i in range(no_stu):
+        for j in range(no_sub):
+            score = eval(input(" "+str(j+1)+" "))
+            
+            
+            
+        
+    
+
+def main():
+    # moredata = "yes"
+    # _sum = 0.0
+    # count = 0
+    # while moredata[0] == 'y':
+    #     x = eval(input("Enter a number >> "))
+    #     _sum = _sum + x
+    #     count = count + 1
+    #     moredata = input("Do you have more numbers (yes or no)? ")
+    nested_loop()
